fd7c55467f77ea7599d9d4340b64108e8478dc67
Tommy <t.a.britnell@gmail.com>
2022-07-23 10:50:27 +0100

    updated demo/app to new usage

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -8,6 +8,7 @@ const Scroller = () => {
   const secRef = useRef<HTMLDivElement>(null);
   const thirdRef = useRef<HTMLDivElement>(null);
   const fourRef = useRef<HTMLDivElement>(null);
+  const allRef = useRef<HTMLDivElement>(null);
   const pacRef = useRef<HTMLDivElement>(null);
   const threshRef = useRef<HTMLDivElement>(null);
   const fadeRef = useRef<HTMLDivElement>(null);
@@ -17,46 +18,61 @@ const Scroller = () => {
   const countRef = useRef<HTMLDivElement>(null);
   const { start } = useCounter();
 
-  useScroller([{ ref: secRef }]);
-  // useScroller([
-  //   {
-  //     ref: elRef,
-  //   },
-  //   { ref: secRef },
-  //   { ref: thirdRef },
-  //   { ref: fourRef },
-  //   {
-  //     ref: pacRef,
-  //     customVars: ({ perc }) => {
-  //       const p = perc * 100;
-  //       const period = 100 / 10;
-  //       const cycle = Math.floor(p / period) % 2 === 0;
-  //       const val = cycle ? p % period : period - (p % period);
-  //       return {
-  //         "--a": val,
-  //       };
-  //     },
-  //   },
-  //   {
-  //     ref: threshRef,
-  //     classChecker: ({ top }) => top > window.innerHeight / 2,
-  //     classTrue: "before",
-  //     classFalse: "after",
-  //     customVars: () => {},
-  //   },
-  //   {
-  //     ref: fadeRef,
-  //     classChecker: ({ top }) => top < window.innerHeight / 2,
-  //     classTrue: "fadeIn",
-  //     customVars: () => {},
-  //   },
-  //   {
-  //     ref: rollRef,
-  //     classChecker: ({ top }) => top < window.innerHeight * 0.75,
-  //     classTrue: "rollIn",
-  //     classFalse: "rollOut",
-  //   },
-  // ]);
+  useScroller({ ref: elRef });
+  useScroller({ ref: secRef });
+  useScroller({ ref: thirdRef });
+  useScroller({ ref: fourRef });
+  useScroller({
+    ref: allRef,
+    customVars: ({ view, scroll, top, bottom }) => {
+      return {
+        "--s": scroll,
+        "--v": view,
+        "--t": top,
+        "--b": bottom,
+      };
+    },
+  });
+
+  useScroller({
+    ref: pacRef,
+    customVars: ({ view, scroll }) => {
+      const p = view * 100;
+      const period = 100 / 10;
+      const cycle = Math.floor(p / period) % 2 === 0;
+      const val = cycle ? p % period : period - (p % period);
+      return {
+        "--v": view,
+        "--a": val,
+      };
+    },
+  });
+
+  useScroller({
+    ref: threshRef,
+    threshold: ({ top }) => ({
+      res: top > 0.5,
+      classTrue: "after",
+      classFalse: "before",
+    }),
+  });
+
+  useScroller({
+    ref: fadeRef,
+    threshold: ({ top }) => ({
+      res: top > 0.5,
+      classTrue: "fadeIn",
+    }),
+  });
+
+  useScroller({
+    ref: rollRef,
+    threshold: ({ top }) => ({
+      res: top > 0.2,
+      classTrue: "rollIn",
+      classFalse: "rollOut",
+    }),
+  });
 
   return (
     <div className="container">
@@ -137,6 +153,12 @@ const Scroller = () => {
       <h2>useScroll</h2>
       <p className="info">A react hook to animate a css variable on Scroll.</p>
       <p className="info">
+        All it needs is a ref to the element you want to be observed. I tend to
+        use a container not the element itself.
+      </p>
+      <p className="code">{`useScroller({ ref: elRef });`}</p>
+
+      <p className="info">
         The basic ( and default) functionality of useScroll is to calculate the
         relative scroll position. By default this is a normalised percentage
         from the moment it is first visible to the moment it scrolls out of view
@@ -148,7 +170,7 @@ const Scroller = () => {
         and
         <br />
         <span className="code">
-          100 = Bottom of element is at (or above) top of scroll window.
+          1.0 = Bottom of element is at (or above) top of scroll window.
         </span>
       </p>
       <p className="info">
@@ -156,23 +178,19 @@ const Scroller = () => {
         the scroll window.
         <br />
         See the rect below. By default this variable is applied as
-        <span className="code">"--p" : [ 0 to 100]</span>
+        <span className="code">"--v" : [ 0 to 1.0 ]</span>
       </p>
       <div ref={elRef} className="sc1"></div>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
         interdum maximus libero, in ornare nunc tincidunt sit amet. Ut efficitur
         maximus molestie. Phasellus sed egestas elit, sed pharetra erat. Vivamus
-        et facilisis dui. Etiam id neque quis sapien rutrum vulputate vitae sit
-        amet tellus. Morbi a feugiat erat, at finibus dui. Fusce feugiat eros in
-        ex mattis tempus. Suspendisse ullamcorper tellus at est tempor,
-        tristique egestas neque hendrerit. Nunc massa leo, sodales at nisl
-        finibus, porta sagittis magna. Nunc ultrices rutrum viverra.
+        et facilisis dui.
       </p>
       <p className="info">
         We can animate or style anything in CSS with this custom prop. <br />
         E.g. move a block from the left to the right. Just transform using
-        <span className="code">var(--p)</span> and use transition to smoothen
+        <span className="code">var(--v)</span> and use transition to smoothen
         out the movement
       </p>
       <p className="code">transition: transform 0.1s linear</p>
@@ -190,32 +208,23 @@ const Scroller = () => {
       </div>
       <h2>Percentage vs Scroll distance</h2>
       <p className="info">
-        As in the example above, when we are animating with
-        <span className="code">--p</span> we are likely to end up in situations
-        where our animation's distance / speed might vary with screen width
-        and/or height.
+        As in the example above, when we are animating with relative values our
+        elements will always go from point A to point B. But this means that the
+        animation's distance / speed will vary with screen width and/or height.
       </p>
       <p className="info">
-        E.g. our ball above is rolling from left to right of screen, during the
-        scroll distance of the screen height. When your animation needs to run
-        at a fixed rate, or with two animations, it is easy for these to go out
-        of sync with varying screen sizes if you dont have fixed distances.
+        E.g. our ball above will always roll from the left to the right side of
+        screen, for the duration of us scrolling. When animations need to run at
+        a fixed rate, or two animations in sync, we can achieve constant
+        movement using the scroll distance
+        <span className="code">--s (scroll)</span> as a fixed distance rather
+        than relative measure. The drawback here is we cannot guarantee the
+        length / distance of the animation, i.e. the ball will not always reach
+        the right side of the screen, or go out of the screen too early.
       </p>
       <p className="info">
-        <span className="code">--p (percentage)</span> is actually calculated
-        from the scrolled distance divided by the full height the item is
-        visible for.
-      </p>
-      <p className="info">
-        To achieve constant movement we can use the scroll distance
-        <span className="code">--s (scroll)</span> to get a fixed distance
-        rather than relative measure. The drawback here is we cannot guarantee
-        the length / distance of the animation, which will vary with screen size
-        and height.
-      </p>
-      <p className="info">
-        Below is the rolling ball animation using --s, see how it is constant
-        over varying screen sizes.
+        So we can rewrite the rolling ball animation using --s, see how it is
+        constant over varying screen sizes.
       </p>
       <div className="second" ref={fourRef}>
         <div className="balltwo"></div>
@@ -226,25 +235,52 @@ const Scroller = () => {
         maximus molestie. Phasellus sed egestas elit, sed pharetra erat. Vivamus
         et facilisis dui.
       </p>
+      <h2>Default vars</h2>
       <p className="info">
-        I know the first thiing you are thinking is : Pacman on scroll
-        animation. And yes, we can do that now.
+        By default <span className="code">view --v</span> and{" "}
+        <span className="code">scroll --s</span> are applied to the element, but
+        you can customise this with the <span className="code">customVars</span>{" "}
+        callback. This receives an additional <span className="code">top</span>{" "}
+        and <span className="code">bottom</span> which vary from 0 to 1 as the
+        top and bottom edge of the element each go from the bottom to the top of
+        the window during scroll. To apply all variables to an element return
+        them from the customVars callback as follows.
       </p>
-      <div ref={pacRef} className="pac-container">
+      <p className="code block">
+        {`useScroller({ 
+  ref: elRef,
+  customVars: ({ view, scroll, top, bottom }) => {
+    return {
+      "--s": scroll,
+      "--v": view,
+      "--t": top,
+      "--b": bottom
+    };
+  },
+});`}
+      </p>
+      <p className="info">
+        Demonstrated aon the object below, inspect the element to watch the
+        behaviour of the various values.
+      </p>
+      <div className="all" ref={allRef}></div>
+
+      <h2>Custom Vars</h2>
+      <p className="info">
+        You can use the same function to apply extra custom variables by
+        returning them from the function. Certain calculations can get tricky in
+        pure CSS when we dont have access to things like '% modulus' operator or
+        trigonometry sin(), cos() etc.
+      </p>
+      <p className="info">
+        I know the first thing you are thinking is : Pacman on scroll animation.
+        And yes, we can do that now. With hardly any js we can make him open and
+        close his mouth repeatedly as he moves over the screen.
+      </p>
+      <div ref={pacRef} className="pacman_container">
         <div className="dots"> </div>
         <div className="pacman"> </div>
       </div>
-      <p className="info">
-        when trying to do more complex animation calculations, like opening and
-        closing Pacman's mouth, this can get tricky in CSS when we dont have
-        access to things like '% modulus' operator or trigonometry sin(), cos().
-      </p>
-      <p className="info">
-        The initalising object for the target can accept a custom function
-        <span className="code">customVars</span> to add extra calculations and
-        custom variables. E.g. above we are calculating an --a from --p, which
-        is then used to animate the mouth angle.
-      </p>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
         interdum maximus libero, in ornare nunc tincidunt sit amet. Ut efficitur
@@ -254,20 +290,37 @@ const Scroller = () => {
       <h2>Thresholds</h2>
       <p className="info">
         In many cases however you might only want to trigger an animation when
-        an item scrolls into view at a particular point.
+        an item scrolls into view at a particular point. For this you can add a
+        callback function to the object called threshold. This receives all the
+        same arguments as the customVars callback above : scroll, view, top &
+        bottom. So you have all the references needed to trigger your change at
+        just the right time.
       </p>
+      <p className="code block">
+        {`useScroller({ 
+  ref: elRef,
+  threshold: ({ top }) => ({
+      res: top > 0.5,
+      classTrue: "rollIn",
+      classFalse: "rollOut",
+    }), 
+});`}
+      </p>
+
       <p className="info">
-        For this you can use
-        <span className="code">classChecker</span>,
-        <span className="code">classTrue</span>&
-        <span className="code">classFalse</span>. classChecker is a callback
-        that will be run on every frame. It must return a boolean that will
-        decide if the true or false classes are applied
+        The callback needs to return an object with a result{" "}
+        <span className="code">res</span> - a boolean that evaluates if the
+        element is above or below the threshold, as well as the classes{" "}
+        <span className="code">classTrue</span> and or{" "}
+        <span className="code">classFalse</span> which should be applied in each
+        case.
       </p>
-      <p className="info"></p>
       <div ref={threshRef} className="thresh"></div>
-      <p className="info">
-        Use this to make elements fade in at specific points.
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+        interdum maximus libero, in ornare nunc tincidunt sit amet. Ut efficitur
+        maximus molestie. Phasellus sed egestas elit, sed pharetra erat. Vivamus
+        et facilisis dui.
       </p>
       <div ref={fadeRef} className="fade">
         <span>KERPOW!</span>
